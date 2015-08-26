873f651a99b1807e5a5e09c45ec4eb54f059470a
Evgeny Shevchenko <shvgn@protonmail.ch>
2015-08-26 17:58:19 +0300

    Elem method and testing in main

--- cmplmat.go
+++ cmplmat.go
@@ -1,10 +1,10 @@
-package cmpxmx2x2
+package main
 
 import (
 	"fmt"
 )
 
-// Matrix of complex numbers
+// Mat is a type of 2×2 matrix of complex numbers
 //  0:m11   1:m12
 //  2:m21   3:m22
 type Mat [4]complex128
@@ -14,6 +14,12 @@ func (m *Mat) String() string {
 	return s
 }
 
+// Elem returns matrix element chosen with (row, col) notation
+func (m *Mat) Elem(r, c int) complex128 {
+	return m[r*2+c]
+}
+
+// Mul multiplies two Mat's
 func Mul(m1, m2 Mat) Mat {
 	var m Mat
 	m[0] = m1[0]*m2[0] + m1[1]*m2[2]
@@ -23,13 +29,20 @@ func Mul(m1, m2 Mat) Mat {
 	return m
 }
 
-// func main() {
-// 	m1 := Mat{1, 2, 3, 4}
-// 	m2 := Mat{3, 1, 4, 2}
+func main() {
 
-// 	fmt.Println(m1.String())
-// 	fmt.Println(m2.String())
-// 	m := Mul(m1, m2)
-// 	fmt.Println(m.String())
+	// This all should be in tests
+	m1 := Mat{1i, 2, 3i, 4}
+	m2 := Mat{3, 1, 4, 2}
 
-// }
+	fmt.Println("M1 =\n" + m1.String() + "\n")
+	fmt.Println("M2 =\n" + m2.String() + "\n")
+	m := Mul(m1, m2)
+	fmt.Println("M = M1 × M2 =\n" + m.String() + "\n")
+
+	fmt.Printf("M(%d, %d) = %v\n", 0, 0, m.Elem(0, 0))
+	fmt.Printf("M(%d, %d) = %v\n", 0, 1, m.Elem(0, 1))
+	fmt.Printf("M(%d, %d) = %v\n", 1, 0, m.Elem(1, 0))
+	fmt.Printf("M(%d, %d) = %v\n", 1, 1, m.Elem(1, 1))
+
+}
